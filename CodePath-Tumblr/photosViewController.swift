50c8c2fcfa0947f82e7b477fb773240be72e4532
Chevy-Personal Mac <chavaneminto@gmail.com>
2017-11-17 21:30:15 -0500

    Grab Photo URLs from API request by index, use AlamoFireImage to grab image from url

--- CodePath-Tumblr/photosViewController.swift
+++ CodePath-Tumblr/photosViewController.swift
@@ -7,13 +7,61 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class photosViewController: UIViewController {
+class photosViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tumblrView: UITableView!
+    
+    var posts: [[String: Any]] = [];
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tumblrView.delegate = self;
+        tumblrView.dataSource = self;
+        
+        tumblrView.rowHeight = 320.0
+        
+        //CREATE A URL REQUEST!!
+        //STEP 1:
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!;
+        //STEP 2:
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main);
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData;
+        //STEP 3:
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDict = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any];
+                let responseDict = dataDict["response"] as! [String:Any];
+                self.posts = responseDict["posts"] as! [[String:Any]];
+                self.tumblrView.reloadData()
+            }
+        }
+        task.resume();
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.posts.count;
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tumblrView.dequeueReusableCell(withIdentifier: "tumblrCell", for: indexPath) as! TumblrCell;
+        
+        let post = posts[indexPath.row];
+        
+        if let photos = post["photos"] as? [[String:Any]] {
+            let photo = photos[0];
+            let originalSize = photo["original_size"] as! [String: Any]
+            let urlString = originalSize["url"] as! String
+            let url = URL(string: urlString)
+            cell.tumblrImage.af_setImage(withURL: url!)
+        }
+        
+        
+        return cell;
     }
 
     override func didReceiveMemoryWarning() {
