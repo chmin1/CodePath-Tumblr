fb310fb3362f08ae9b929d24858ae7a771d28b5c
Chevy-Personal Mac <chavaneminto@gmail.com>
2017-11-17 21:09:20 -0500

    Added tableview and tableviewcell with an imageview

---
+++ CodePath-Tumblr/TumblrCell.swift
@@ -0,0 +1,24 @@
+//
+//  TumblrCell.swift
+//  CodePath-Tumblr
+//
+//  Created by Chavane Minto on 11/17/17.
+//  Copyright © 2017 Chavane Minto. All rights reserved.
+//
+
+import UIKit
+
+class TumblrCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
