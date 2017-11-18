65bd7a5da1395493e77540833f12301ad25b0bd4
Chevy-Personal Mac <chavaneminto@gmail.com>
2017-11-17 21:20:33 -0500

    Created class for reusable tableViewCell

--- CodePath-Tumblr/TumblrCell.swift
+++ CodePath-Tumblr/TumblrCell.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class TumblrCell: UITableViewCell {
-
+    
+    @IBOutlet weak var tumblrImage: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
